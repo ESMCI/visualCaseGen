2b90a0cae2ecb2d158f45998b1be602c143b3599
alperaltuntas <alperaltuntas@gmail.com>
2021-01-15 17:59:24 -0700

    add logger to ConfigVar

--- visualCIME/ConfigVar.py
+++ visualCIME/ConfigVar.py
@@ -1,5 +1,7 @@
-import time
+import logging
 from visualCIME.visualCIME.OutputVC import get_output_widget
+
+logger = logging.getLogger(__name__)
 
 c_base = int("1F534",base=16)
 
@@ -12,11 +14,14 @@ class ConfigVar:
     compliances = None
 
     def __init__(self, name):
+        if name in ConfigVar.vdict:
+            logger.warning("ConfigVar {} already created.".format(name))
         self.name = name
         self.options = []
         self.widget = None
         self.errMsgs = []
         ConfigVar.vdict[name] = self
+        logger.debug("ConfigVar {} created.".format(self.name))
 
     def get_value(self, strip_stat=False):
         assert self.widget != None, "Cannot determine value for "+self.name+". Associated widget not initialized."
@@ -33,6 +38,8 @@ class ConfigVar:
 
     def assign_states(self, change=None):
 
+        logger.debug("Assigning the states of options for ConfigVar {}".format(self.name))
+
         #if change and change['new'].split()[0] == chr(int("1F534",base=16)):
         #    return # invalid selection from source. do nothing. invalid selection will be rejected
 
@@ -40,49 +47,50 @@ class ConfigVar:
 
         new_widget_options = []
         self.errMsgs = []
-        with get_output_widget():
-            for i in range(len(self.options)):
-                option = self.options[i]
+        for i in range(len(self.options)):
+            option = self.options[i]
+            logger.debug("ConfigVar {} option: {}".format(self.name, option))
 
-                def instance_val_getter(cvName):
-                    if cvName==self.name:
-                        return option
-                    else:
-                        return ConfigVar.vdict[cvName].get_value(strip_stat=True)
+            def instance_val_getter(cvName):
+                if cvName==self.name:
+                    return option
+                else:
+                    return ConfigVar.vdict[cvName].get_value(strip_stat=True)
 
-                status, errMsg = True, ''
-                for implication in self.compliances.implications(self.name):
-                    try:
-                        self.compliances.check_implication(
-                            implication,
-                            instance_val_getter
-                            )
-                    except AssertionError as e:
-                        errMsg = e
-                        status = False
-                        break
-                self.errMsgs.append(errMsg)
-                new_widget_options.append('{}  {}'.format(chr(c_base+status), option))
+            status, errMsg = True, ''
+            for implication in self.compliances.implications(self.name):
+                try:
+                    self.compliances.check_implication(
+                        implication,
+                        instance_val_getter
+                        )
+                except AssertionError as e:
+                    errMsg = e
+                    status = False
+                    break
+            self.errMsgs.append(errMsg)
+            new_widget_options.append('{}  {}'.format(chr(c_base+status), option))
 
-            self.widget.options = new_widget_options
-            self.widget.value = None # this is needed here to prevent a weird behavior:
-                                     # in absence of this, widget selection clears for
-                                     # some reason
-            self.widget.value = self.widget.options[old_value_index]
+        self.widget.options = new_widget_options
+        self.widget.value = None # this is needed here to prevent a weird behavior:
+                                 # in absence of this, widget selection clears for
+                                 # some reason
+        self.widget.value = self.widget.options[old_value_index]
 
     def update_states(self, change):
         with get_output_widget():
-            print("updating states")
-        self.assign_states(change)
+            logger.debug("Updating the states of options for ConfigVar {}".format(self.name))
+            self.assign_states(change)
 
     def check_selection_validity(self, change):
         with get_output_widget():
+            logger.debug("Checking option validity for ConfigVar {}".format(self.name))
             if 'index' in change['new']:
                 new_index = change['new']['index']
                 new_val = self.widget.options[new_index]
                 if new_val.split()[0] == chr(int("1F534",base=16)):
-                    print("ERROR: invalid ",self.name)
-                    print("\t",self.errMsgs[new_index])
+                    logger.critical("Invalid selection for {}".format(self.name))
+                    logger.critical(self.errMsgs[new_index])
                 else:
                     get_output_widget().clear_output()
 
