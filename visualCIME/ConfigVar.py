b7c18653676ea12a482420225368387aacdca0bf
alperaltuntas <alperaltuntas@gmail.com>
2021-01-18 20:52:28 -0700

    fix check_selection_validity

--- visualCIME/ConfigVar.py
+++ visualCIME/ConfigVar.py
@@ -4,7 +4,7 @@ import ipywidgets as widgets
 
 logger = logging.getLogger(__name__)
 
-c_base = int("1F534",base=16)
+c_base_red = int("1F534",base=16)
 
 class ConfigVar:
 
@@ -24,6 +24,16 @@ class ConfigVar:
         ConfigVar.vdict[name] = self
         logger.debug("ConfigVar {} created.".format(self.name))
 
+    @staticmethod
+    def value_is_valid(val):
+        val_split = val.split()
+        if len(val_split)>=2:
+            if val_split[0] == chr(c_base_red):
+                return False
+            elif val_split[0] == chr(c_base_red+True):
+                return True
+        else:
+            raise RuntimeError("Corrupt value passed to value_is_valid(): {}".format(val))
     def get_value(self, strip_stat=False):
         assert self.widget != None, "Cannot determine value for "+self.name+". Associated widget not initialized."
         if strip_stat:
@@ -44,7 +54,7 @@ class ConfigVar:
             logger.debug("change: {}".format(change))
         if change != None:
             assert isinstance(change['owner'], widgets.Select)
-            if change['owner'].value.split()[0] == chr(int("1F534",base=16)):
+            if not ConfigVar.value_is_valid(change['owner'].value):
                 with get_output_widget():
                     logger.debug("Invalid selection from owner. Do nothing for ConfigVar {}".format(self.name))
                 return
@@ -80,7 +90,7 @@ class ConfigVar:
                         status = False
                         break
                 self.errMsgs.append(errMsg)
-                new_widget_options.append('{}  {}'.format(chr(c_base+status), option))
+                new_widget_options.append('{}  {}'.format(chr(c_base_red+status), option))
                 logger.debug("ConfigVar {} option: {}, status: {}".format(self.name, option, status))
 
         self.widget.options = new_widget_options
@@ -98,15 +108,39 @@ class ConfigVar:
             raise NotImplementedError
 
     def check_selection_validity(self, change):
+
         with get_output_widget():
-            logger.debug("Checking option validity for ConfigVar {}".format(self.name))
-            if 'index' in change['new']:
-                new_index = change['new']['index']
-                new_val = self.widget.options[new_index]
-                logger.debug("New option val: {}".format(new_val))
-                if new_val.split()[0] == chr(int("1F534",base=16)):
+            logger.debug("Checking the validity for ConfigVar {} with value={}".format(self.name, self.widget.value))
+
+        if change != None:
+            assert change['name'] == 'value'
+            new_val = change['new']
+            if new_val != None and not ConfigVar.value_is_valid(new_val):
+                with get_output_widget():
+                    new_index = self.get_value_index()
+                    #get_output_widget().clear_output()
                     logger.critical("ERROR: Invalid selection for {}".format(self.name))
                     logger.critical(self.errMsgs[new_index])
-                else:
-                    get_output_widget().clear_output()
+                self.widget.value = change['old']
+        else:
+            raise NotImplementedError
+
+                #new_index = change['new']['index']
+                #new_val = self.widget.options[new_index]
+                #logger.debug("New option val: {}".format(new_val))
+                #if new_val.split()[0] == chr(int("1F534",base=16)):
+                    #logger.critical("ERROR: Invalid selection for {}".format(self.name))
+                    #logger.critical(self.errMsgs[new_index])
+                    #selection_valid = False
+                #else:
+                    #get_output_widget().clear_output()
+        #if not selection_valid:
+            #old_index = self.get_value_index()
+            #with get_output_widget():
+                #logger.debug("setting ConfigVar {} value to {}".format(self.name, self.widget.options[old_index]))
+            #self.widget.value = None
+            #self.widget.value = self.widget.options[old_index]
+            #with get_output_widget():
+                #print("dbg:", self.widget.value)
+
 
