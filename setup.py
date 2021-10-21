fdc9f6652e33aed894b0efc2695b6a818f515f27
alperaltuntas <alperaltuntas@gmail.com>
2021-10-21 13:37:09 -0600

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,34 +1,3 @@
-import distutils
-import setuptools
-import subprocess
-from distutils.command.build import build as _build
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="visualCaseGen",
-    version="0.0.1",
-    author="Alper Altuntas",
-    author_email="altuntas@ucar.edu",
-    description="A JupyterLab based Graphical User Interface for CESM create_newcase tool.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ESMCI/visualCaseGen",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/visualCaseGen/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: LGPL-3 License",
-        "Operating System :: Unix-like",
-    ],
-    package_dir={"": "visualCaseGen"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
-    install_requires=[
-        'jupyterlab>3.2.0,<4',
-        'ipywidgets>=7.6.5,<8',
-        'PyYAML>=5.4,<6'
-    ],
-)
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
