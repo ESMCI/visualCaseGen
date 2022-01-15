0c6d89af744df0a59bf8ae313ee4808bbea83b9e
alperaltuntas <alperaltuntas@gmail.com>
2022-01-15 09:52:25 -0700

    add traits to dummywidget

--- visualCaseGen/dummy_widget.py
+++ visualCaseGen/dummy_widget.py
@@ -1,12 +1,16 @@
-class DummyWidget():
+from traitlets import HasTraits, Any
+
+class DummyWidget(HasTraits):
     """A dummy class to be used as a placeholder. By default, all ConfigVar widgets are set to a DummyWidget instance
     at initialization. A ConfigVar widget is typically reassigned to an actual widget when it is ready."""
 
-    def __init__(self, value=None, options=None, description="DummyWidget"):
-        self.value = value
-        self.options = options
-        self.description = description
+    value = Any()
+    options = Any((), help="Iterable of values, (label, value) pairs, or a mapping of {label: value} pairs that the "
+                            "user can select.")
 
-    def observe(*args, **kwargs):
-        """A dummy observe method that does nothing. Used as a placeholder"""
-        pass
+    def __init__(self, value=None, options=None, description="DummyWidget"):
+        self.description = description
+        if value:
+            self.set_trait('value',value)
+        if options:
+            self.set_trait('options',options)
