6d2e5542fa9cdf77b775da1d127d7cb81300eec9
alperaltuntas <alperaltuntas@gmail.com>
2022-03-21 19:10:22 -0600

    introduce the dev_utils module

---
+++ visualCaseGen/dev_utils.py
@@ -0,0 +1,17 @@
+
+# debug flag, turns on some checks during runtime
+debug = True
+
+# List of all ConfigVar assignments in chronological order. Used for debugging purposes
+assignment_history = []
+
+class RunError(Exception):
+
+    def __init__(self, message="", print_assignment_hist=True):
+        self.message = message
+        print("A runtime error encountered. Here is the variable assignment that led to the error:")
+        if print_assignment_hist:
+            for assignment in assignment_history:
+                print(assignment)
+        super().__init__(self.message)
+
